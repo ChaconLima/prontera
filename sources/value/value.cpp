cbb656c4e392d074af7e095820f32e9da50c6388
ChaconLima <mateusdeassislima@gmail.com>
2022-12-18 22:15:40 -0300

    fix: starter class removal

--- sources/value/value.cpp
+++
@@ -1,8 +0,0 @@
-#include "value.h"
-
-namespace value{
-    std::string getValue(){
-        return "test_cmake";
-    }
-} 
-
--- standalone/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <value/value.h>
-#include <iostream>
-
-int main(){
-    std::cout<<value::getValue()<<"\n";
-    return 0;
-}
